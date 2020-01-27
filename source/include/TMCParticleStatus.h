19e3ac84777c1cb751231625d865ca0f5de37423
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-01-27 12:49:57 +0100

    Make TMCParticleStatus.h identical with the file in ROOT:
    This applies a fix in Info scope() and reverts changes in the class description which diverged.
    
    Details:
    Fix from Root (3e2cb48):
    Re-apply: Disambiguate ROOT's ::Info from Pythia8's Info:
    
    Some Pythia8 headers expose an unfortunate "using namespace Pythia8".
    This triggers:
    In file included from /root/ship-log-keep/sw/slc7_x86-64/ROOT/master_FAIRSHIP-1/include/TMCManager.h:26:
    /root/ship-log-keep/sw/slc7_x86-64/ROOT/master_FAIRSHIP-1/include/TMCParticleStatus.h:81:7: error: reference to 'Info' is ambiguous
          Info("Print", "Status of track");
          ^
    /root/ship-log-keep/sw/BUILD/b61305e07edfb1f9a906335e2093ac409db3c4cc/ROOT/include/TError.h:58:13: note: candidate found by name lookup is 'Info'
    extern void Info(const char *location, const char *msgfmt, ...)
                ^
    /root/ship-log-keep/sw/slc7_x86-64/pythia/v8230-ship-1/include/Pythia8/Info.h:30:7: note: candidate found by name lookup is 'Pythia8::Info'
    class Info {
          ^
    
    By explicitly requesting "::Info" that ambiguity should be resolved.

--- source/include/TMCParticleStatus.h
+++ source/include/TMCParticleStatus.h
@@ -20,11 +20,10 @@
 #ifndef ROOT_TMCParticleStatus
 #define ROOT_TMCParticleStatus
 
-/** \class TMCParticleStatus
-    \ingroup vmc
-
-Additional information on the current status of a TParticle
-*/
+// Class TMCParticleStatus
+// ---------------------
+// additional information on the current status of a TParticle
+//
 
 #include <iostream>
 
@@ -56,7 +55,7 @@ struct TMCParticleStatus {
    /// Print all info at once
    void Print() const
    {
-      Info("Print", "Status of track");
+      ::Info("Print", "Status of track");
       std::cout << "\t"
                 << "ID: " << fId << "\n"
                 << "\t"
